109c2ab08bc58ebf0ee0c36c7e42d0241b15c4c4
kpdrozag <kevin.andres01@hotmail.com>
2018-05-24 15:43:54 -0500

    Good

--- server.py
+++ server.py
@@ -1,21 +1,3 @@
-"""
-(5%) En un nuevo repositorio crea un proyecto Flask
-
-(20%) La app debe responder a las ruta /hash/<string> que recibe como parámetro un string,
-    retornando su respectivo valor (SHA256, SHA384, SHA224, SHA512, SHA1) en un objeto
-    json descrito así: { “sha256”: xxxx, “sha512”: xxxx, … }.
-
-(20%) Al momento de responder a las peticiones sin /json use una plantilla HTML5 con Bootstrap.
-
-(20%) Por la aplicación intencional de técnicas avanzadas de POO (solid, patrones, etc)
-
-(30%)  El servicio al recibir una petición en  “/” retorna:
-Nombre completo del estudiante
-Diagrama de clases del servicio
-Diagrama de Secuencias.
-
-(5%) Suba todos los cambios faltantes a Github y comparte la URL como respuesta a este parcial.
-"""
 import hashlib as hs
 from flask import Flask, render_template, jsonify
 
@@ -51,8 +33,7 @@ def hash_json(string):
                    sha384=lista[1][1],
                    sha224=lista[2][1],
                    sha512=lista[3][1],
-                   sha1=lista[4][1]
-                   )
+                   sha1=lista[4][1])
 
 
 if __name__ == '__main__':
